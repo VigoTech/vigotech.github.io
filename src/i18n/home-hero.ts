83c8f6af21066f2fd221b71e1096ee80f76eb787
Miguel Gonzalez <migonzalvar@gmail.com>
2026-04-08 19:22:40 +0200

    chore: typos

--- src/i18n/home-hero.ts
+++ src/i18n/home-hero.ts
@@ -2,9 +2,9 @@ export const homeHeroT = {
   gl: {
     titleMarkdown: 'Os grupos de tecnoloxía de Vigo **facemos piña**',
     description:
-      'Un ecosistema federado de comunidades tecnicas, hackers e innovadores. Unimos precision tecnica e conexion humana para visibilizar o movemento local.',
+      'Un ecosistema federado de comunidades técnicas, hackers e innovadores. Unimos precisión técnica e conexión humana para visibilizar o movemento local.',
     exploreButton: 'Explora o ecosistema',
-    upcomingEvents: 'Proximos eventos',
+    upcomingEvents: 'Próximos eventos',
     viewEvent: 'Ver evento',
   },
 } as const
